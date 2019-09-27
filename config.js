cc8f45d1ad416fd0ee47bf4069838dac393c5c5a
liao <mail@liaozhibin.com>
2019-09-27 18:37:26 +0800

    save

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * 配置页面路径
+ */
+
+export default {
+  blogDir: './blog',
+  blogLayout: './layouts/blog', // contain files: blog.js blog.ejs blog.scss
+  homeLayout: './layouts/home',
+  aboutLayout: './layouts/about'
+}
