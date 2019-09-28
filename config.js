6c85f8c6248e9522474e39017919477e4534521e
liao <mail@liaozhibin.com>
2019-09-29 02:18:10 +0800

    生成 html 模板

--- config.js
+++ config.js
@@ -2,9 +2,19 @@
  * 配置页面路径
  */
 
-export default {
-  blogDir: './blog',
-  blogLayout: './layouts/blog', // contain files: blog.js blog.ejs blog.scss
-  homeLayout: './layouts/home',
-  aboutLayout: './layouts/about'
+module.exports = {
+  pages: {
+    home: {
+      layout: './layouts/home',
+      content: './README.md'
+    },
+    blog: {
+      layout: './layouts/blog',
+      content: 'blog'
+    },
+    about: {
+      layout: './layouts/about',
+      content: './README.md'
+    }
+  }
 }
