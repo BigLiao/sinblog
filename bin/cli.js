18814c20fb1b28e502ca3a929d608a30db295fa3
liao <mail@liaozhibin.com>
2019-10-18 14:17:17 +0800

    大力开发中

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
 const pkg = require('../package.json');
 const program = require('commander');
 
@@ -12,28 +11,28 @@ program
   .command('init')
   .description('Initial sinblog configs')
   .action((value, cmd) => {
-    require('../lib/cli/init')(value, cmd);
+    require('../lib/init/cli')(value, cmd);
   });
 
 program
   .command('build')
   .description('Generate static website')
   .action((value, cmd) => {
-    require('../lib/cli/build')(value, cmd);
+    require('../lib/build/cli')(value, cmd);
   });
 
 program
   .command('publish')
   .description('Publish website')
   .action((value, cmd) => {
-    require('../lib/cli/publish')(value, cmd);
+    require('../lib/publish/cli')(value, cmd);
   });
 
 program
   .command('serve')
   .description('Run web server locally')
   .action((value, cmd) => {
-    require('../lib/cli/serve')(value, cmd);
+    require('../lib/serve/cli')(value, cmd);
   });
 
 
---
+++ test/test1.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+const a = './docs/blog';
+console.log(me(a));
